10af6033fe61673d823fe502024f2aaea5364bc8
yunma-code <ma.yun2@northeastern.edu>
2025-02-12 00:22:19 -0500

    init-navbar

---
+++ frontend/src/components/navbar/navbar.tsx
@@ -0,0 +1,30 @@
+// create navigation bar
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+        
+    <Navbar bg="dark" data-bs-theme="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Home
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/about">
+              About
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
+
