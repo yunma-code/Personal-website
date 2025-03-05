3d9d8f46db698aadfc54d01c16448257efbd8bd5
yunma-code <ma.yun2@northeastern.edu>
2025-03-05 17:20:34 -0500

    change panel direction

--- frontend/src/components/home/home.tsx
+++ frontend/src/components/home/home.tsx
@@ -1,32 +1,61 @@
 import React, { useState } from "react";
 import "./home.css";
 
-const Home: React.FC =() => {
+const Home: React.FC = () => {
 
-	return(
+	return (
 		<div className="home-container">
-			<div className="content-box">
+			<div className="left-box">
 				<div className="content-text">
 					<h1>
 						Yun Ma
 						<h4>Bridging music and tech</h4>
 					</h1>
 					<br />
-					<h5>
-					I'm a software developer and musician based in Boston, passionate about  <br />
-					<span style={{ color: '#2980b9', fontWeight: 'bold'}}> Web development</span>,
-					<span style={{ color: '#2980b9', fontWeight: 'bold'}}> Cloud computing</span>,
-					<span style={{ color: '#2980b9', fontWeight: 'bold'}}> Machine Learning </span> 
-					and <span style={{ color: '#2980b9', fontWeight: 'bold'}}> Music Technology.</span>
-					
-					<br />Check out my work in both coding and music composition!
-					</h5>
 				</div>
-			</div>
-		
+				<div className="navigation-links">
+					<ul>
+						<li>
+							<a href="#technical-skills">Technical Skills</a>
+						</li>
+						<li>
+							<a href="#projects">Projects</a>
+						</li>
+						<li>
+							<a href="#education">Education</a>
+						</li>
+					</ul>
+				</div>
 
-			<div className="technical-box">
-				<div className="technical-text">
+			</div>
+
+
+			<div className="right-box">
+				<div className="content-text">
+					<h5>
+						I'm a software developer and musician based in Boston, passionate
+						about{" "}
+						<span style={{ color: "#2980b9", fontWeight: "bold" }}>
+							{" "}
+							Web development
+						</span>
+						, <span style={{ color: "#2980b9", fontWeight: "bold" }}>
+							Cloud computing
+						</span>
+						,{" "}
+						<span style={{ color: "#2980b9", fontWeight: "bold" }}>
+							Machine Learning
+						</span>{" "}
+						and{" "}
+						<span style={{ color: "#2980b9", fontWeight: "bold" }}>
+							Music Technology.
+						</span>
+						<br />
+						Check out my work in both coding and music composition!
+					</h5>
+					<br />
+				</div>
+				<div id="technical-skills" className="technical-text">
 					<h3>Technical Skills</h3>
 					<div>Programming Languages: Java, JavaScript, TypeScript, C++, Python, SQL, HTML/CSS</div>
 					<div>Frameworks & Libraries: React, Node.js, REST APIs</div>
@@ -35,10 +64,22 @@ const Home: React.FC =() => {
 					<div>Tools & DevOps: Linux, Bash, Git, CMake, Makefile</div>
 					<div>Machine Learning: PyTorch, Numpy, scikit-learn, pandas, Matplotlib</div>
 				</div>
+
+				<div id="projects" className="projects-text">
+					<h3>Projects</h3>
+					<div>Project 1: </div>
+					<div>Project 2: </div>
+					<div>Project 3: </div>
+					
+				</div>
+
+				<div id="education" className="education-text">
+					<h3>Education</h3>
+					<div>Degree:</div>
+					<div>Year: </div>
+				</div>
+
 			</div>
-
-
-
 		</div>
 	);
 };
