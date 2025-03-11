171018ac80afcb851820ee1e08058b08eb3e77d3
yunma-code <ma.yun2@northeastern.edu>
2025-03-11 02:31:09 -0400

    fix server side

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
