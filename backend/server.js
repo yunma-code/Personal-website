e724cfc2b5f8a2b17bbe37948f8e80334e87b065
yunma-code <ma.yun2@northeastern.edu>
2025-03-11 02:50:02 -0400

    fix server

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,6 @@ app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
-
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${port}`);
   });
