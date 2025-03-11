2115e5e6f69fe91ad86924aceb67d27084842f62
yunma-code <ma.yun2@northeastern.edu>
2025-03-11 01:41:14 -0400

    update server file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-require('dotenv').config();
-
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
