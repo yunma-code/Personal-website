99f68b94dd0634124f1300179fa5a1decf04addc
yunma-code <ma.yun2@northeastern.edu>
2025-03-11 02:46:39 -0400

    fix server

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config();
 
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());    
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('/', (req, res) => {
@@ -15,3 +18,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running at http://0.0.0.0:${port}`);
+  });
+  
